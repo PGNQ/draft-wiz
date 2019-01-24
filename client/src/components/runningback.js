c757b17856a3d4a1eefc525d918e1ca92673a760
PGNQ <41378170+PGNQ@users.noreply.github.com>
2019-01-24 18:28:03 -0500

    Fix styling of input fields

--- client/src/components/runningback.js
+++ client/src/components/runningback.js
@@ -12,63 +12,65 @@ class Runningback extends Component {
     return (
       <div className="container content">      
          <img className="mx-auto d-block" src="https://images-na.ssl-images-amazon.com/images/I/417Yg63btmL._SX425_.jpg"  alt="runningback"></img>
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">Height (in)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-            placeholder='Ave: 71'/>
+         <div className="container input-container">
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Height (in)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 71'/>
+          </div>
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Weight (lbs)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 214'/>
+          </div>  
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">40-yard dash (sec)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 4.5'/>
+          </div>  
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">20-yard shuttle (sec)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"  
+              placeholder='Ave: 4.2'/>
+          </div>  
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">3 cone drill (sec)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 7.0'/>
+          </div>  
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Vertical jump (in)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 35'/>
+          </div>  
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Broad jump (in)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 119'/>
+          </div>   
+          <div className="input-group input-group-sm mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="inputGroup-sizing-sm">Bench press (reps)</span>
+              </div>
+              <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
+              placeholder='Ave: 20'/>
+          </div>   
+          <button type="button" className="btn btn-success">Predict!</button> 
          </div>
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">Weight (lbs)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-            placeholder='Ave: 214'/>
-         </div>  
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">40-yard dash (sec)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-             placeholder='Ave: 4.5'/>
-         </div>  
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">20-yard shuttle (sec)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"  
-            placeholder='Ave: 4.2'/>
-         </div>  
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">3 cone drill (sec)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-             placeholder='Ave: 7.0'/>
-         </div>  
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">Vertical jump (in)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-             placeholder='Ave: 35'/>
-         </div>  
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">Broad jump (in)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-             placeholder='Ave: 119'/>
-         </div>   
-         <div className="input-group input-group-sm mb-3">
-            <div className="input-group-prepend">
-               <span className="input-group-text" id="inputGroup-sizing-sm">Bench press (reps)</span>
-            </div>
-            <input type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"
-             placeholder='Ave: 20'/>
-         </div>   
-         <button type="button" className="btn btn-success">Predict!</button> 
       </div>
     );
   }
